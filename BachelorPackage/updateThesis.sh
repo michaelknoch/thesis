b72bf12362d3904abf6da9cf2bb791370065955d
Michael Knoch <info@michaelknoch.de>
2016-05-30 13:19:07 +0200

    add update script

---
+++ BachelorPackage/updateThesis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+echo "moving bachelorthesis.zip from downloads into current directory"
+mv ~/Downloads/bachelorthesis.zip ./
+if [ $? -eq 0 ]; then
+    echo "unzipping into ./writing"
+    unzip -uo bachelorthesis.zip -d ./
+
+
+    echo "moving zip into prevImports with current timestamp"
+    mv bachelorthesis.zip ../prevImports/$(timestamp).zip
+
+    git status
+else
+    echo "there was no bachelorthesis.zip in your Downloads directory, exiting"
+fi

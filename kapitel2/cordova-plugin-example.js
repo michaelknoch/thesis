129333013924f2307165d15374e89032d9e32e01
Michael Knoch <info@michaelknoch.de>
2016-07-07 18:17:50 +0200

    stuff

---
+++ kapitel2/cordova-plugin-example.js
@@ -0,0 +1,11 @@
+navigator.camera.getPicture(onSuccess, onFail, { quality: 90,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
